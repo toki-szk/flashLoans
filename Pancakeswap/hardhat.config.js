3a5e8ac8e45c5548df42d3da72fd8b8cce531eed
toki suzuki <tokinoshin.suzuki@pilotz.jp>
2022-07-02 15:42:30 +0900

    [Add] network

--- Pancakeswap/hardhat.config.js
+++ Pancakeswap/hardhat.config.js
@@ -24,4 +24,20 @@ module.exports = {
       { version: "0.8.8" },
     ],
   },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://bsc-dataseed1.binance.org/",
+      },
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      accounts: [""],
+    },
+    mainnet: {
+      url: "https://bsc-dataseed1.binance.org/",
+      chainId: 56,
+    },
+  },
 };
